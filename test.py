c332e338c6f4f86aa458265cf3a7216a52ea314c
Ines Montani <ines@ines.io>
2019-01-31 14:50:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def lang():
 def test_spacy_stanfordnlp(lang, models_dir):
     snlp = stanfordnlp.Pipeline(lang=lang, models_dir=models_dir)
     nlp = StanfordNLPLanguage(snlp)
-    assert nlp.lang == lang
+    assert nlp.lang == "stanfordnlp_" + lang
 
     doc = nlp("Hello world! This is a test.")
 
@@ -29,7 +29,14 @@ def test_spacy_stanfordnlp(lang, models_dir):
     assert [t.dep_ for t in doc] == ["root", "vocative", "punct", "nsubj", "cop", "det", "root", "punct"]
     assert [t.is_sent_start for t in doc] == [True, None, None, True, None, None, None, None]
     assert [t.is_stop for t in doc] == [False, False, False, True, True, True, False, False]
+    # fmt: on
     assert len(list(doc.sents)) == 2
     assert doc.is_tagged
     assert doc.is_parsed
     assert doc.is_sentenced
+
+    docs = list(nlp.pipe(["Hello world", "This is a test"]))
+    assert docs[0].text == "Hello world"
+    assert [t.pos_ for t in docs[0]] == ["INTJ", "NOUN"]
+    assert docs[1].text == "This is a test"
+    assert [t.pos_ for t in docs[1]] == ["DET", "VERB", "DET", "NOUN"]
