d575d2cbb3f59a751f2cf09eedb0e18c3c3db001
Ines Montani <ines@ines.io>
2019-01-31 14:53:14 +0100

    Require latest stable spaCy instead of nightly

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def setup_package():
         version=about["__version__"],
         license=about["__license__"],
         packages=find_packages(),
-        install_requires=["spacy-nightly>=2.1.0a5", "stanfordnlp>=0.1.0,<0.2.0"],
+        install_requires=["spacy>=2.0.18", "stanfordnlp>=0.1.0,<0.2.0"],
         python_requires=">=3.6",
         entry_points={
             "spacy_languages": [
