aa7371165778cc281536491f572eb3ff71f3c5f3
Ines Montani <ines@ines.io>
2019-07-09 22:05:12 +0200

    Increment version

--- spacy_stanfordnlp/about.py
+++ spacy_stanfordnlp/about.py
@@ -1,5 +1,5 @@
 __title__ = "spacy-stanfordnlp"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __summary__ = "Use the latest StanfordNLP research models directly in spaCy"
 __uri__ = "https://explosion.ai"
 __author__ = "Ines Montani"
