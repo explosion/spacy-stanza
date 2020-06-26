fcfc4f4f12597cf2854da0eeeedbc19bd48fc7c2
Ines Montani <ines@ines.io>
2020-06-26 10:39:29 +0200

    Increment version

--- spacy_stanza/about.py
+++ spacy_stanza/about.py
@@ -1,5 +1,5 @@
 __title__ = "spacy-stanza"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __summary__ = "Use the latest Stanza (StanfordNLP) research models directly in spaCy"
 __uri__ = "https://explosion.ai"
 __author__ = "Ines Montani"
