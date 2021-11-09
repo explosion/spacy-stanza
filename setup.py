b9495a2deeabc98b0a271f8373304f864f5d6383
Adriane Boyd <adrianeboyd@gmail.com>
2021-11-09 10:29:04 +0100

    Add classifier for python 3.10

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ def setup_package():
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
         ],
         zip_safe=False,
     )
